65705ba04c7b03bead49c15fd6d8185bcc7545e9
RAMZAN PRO <ramzanpro10@gmail.com>
2021-10-13 00:45:26 +0600

    first commit

---
+++ root/main.js
@@ -0,0 +1,34 @@
+alert('Fight');
+console.log('Fight');
+let start = 'skorpion';
+let game = "sab zero";
+console.log(typeof start);
+console.log(typeof game);
+
+let wander = 6;
+console.log(typeof wander);
+let heppy = 4 / 0;
+let dark = 10 * 'Baran';
+console.log(heppy);
+console.log(dark);
+friz = null;
+console.log(friz);
+console.log(typeof friz);
+bob = 44 + 'mista';
+console.log(bob);
+let obj = {};
+console.log(typeof obj);
+king = {} + 'magik';
+console.log(king);
+console.log(typeof king);
+red = function myFunction() { };
+console.log(red);
+console.log(typeof red);
+
+let magyc = '' + 2590;
+console.log(magyc);
+console.log(typeof magyc);
+
+let tor = String(5532);
+console.log(tor);
+console.log(typeof tor);
